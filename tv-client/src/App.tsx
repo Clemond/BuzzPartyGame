2552b537679e358f928985240cdb8cb4f4f081ec
Nicholas <quackshows11@gmail.com>
2025-06-25 10:05:59 +0200

    feat: add basic design to tv-client

--- tv-client/src/App.tsx
+++ tv-client/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [gameCode, setGameCode] = useState<string | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Lobby</h1>
+      <p>Join Code: {gameCode || "Generating..."}</p>
     </div>
   );
 }
-
-export default App;
