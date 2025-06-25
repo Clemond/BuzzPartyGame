f0d5fd5f6600973e26e346a9f64441b857ee5d67
Nicholas <quackshows11@gmail.com>
2025-06-25 10:28:24 +0200

    feat: add logic to get game code and display it in tv-client

--- tv-client/src/App.tsx
+++ tv-client/src/App.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 export default function App() {
   const [gameCode, setGameCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    socket.on("gameCreated", (code: string) => {
+      setGameCode(code);
+    });
+
+    return () => {
+      socket.off("gameCreated");
+    };
+  }, []);
 
   return (
     <div className="Container">
